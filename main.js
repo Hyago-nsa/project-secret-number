4ac030e2bba40ec72125921177839a789cc65c97
Hyago-nsa <80161402+Hyago-nsa@users.noreply.github.com>
2023-03-06 17:21:54 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const menorValor = 1
+const maiorValor = 1000
+
+const secretNumber = Math.round(Math.random() * maiorValor + 1)
+
+const elementMaiorValor = document.getElementById("maior-valor")
+elementMaiorValor.innerHTML = maiorValor
+const elementMenorValor = document.getElementById("menor-valor")
+elementMenorValor.innerHTML = menorValor
+
+console.log(secretNumber)
